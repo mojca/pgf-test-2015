ef05a36f6df135096c8815d0f5c6850dfebb92f3
Jannis Pohlmann <jannis-pohlmann@users.sourceforge.net>
2011-05-02 03:05:04 +0000

    - Implement coarsening in the Walshaw algorithm. Add Edge:__eq().

--- generic/pgf/libraries/graphdrawing/lua/pgflibrarygraphdrawing-node.lua
+++ generic/pgf/libraries/graphdrawing/lua/pgflibrarygraphdrawing-node.lua
@@ -8,7 +8,7 @@
 --
 -- See the file doc/generic/pgf/licenses/LICENSE for more information
 
--- @release $Header: /cvsroot/pgf/pgf/generic/pgf/libraries/graphdrawing/lua/pgflibrarygraphdrawing-node.lua,v 1.8 2011/05/02 02:25:23 jannis-pohlmann Exp $
+-- @release $Header: /cvsroot/pgf/pgf/generic/pgf/libraries/graphdrawing/lua/pgflibrarygraphdrawing-node.lua,v 1.9 2011/05/02 02:47:03 jannis-pohlmann Exp $
 
 -- This file defines a node class, used in the graph representation.
 
@@ -160,7 +160,6 @@ end
 -- @param object The node to be compared to self
 -- @return True if self is equal to object.
 function Node:__eq(object)
-   Sys:logMessage('LAY: node eq function called')
    return self.name == object.name
 end
 
