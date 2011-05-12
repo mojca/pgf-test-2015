463dc88b0f1aee78733824265098fad026d52492
Jannis Pohlmann <jannis-pohlmann@users.sourceforge.net>
2011-05-12 02:10:37 +0000

    graph drawing:   - Add Fibonacci heap and priority queue classes.   - Add Lua file for common graph algorithms. Implement Dijkstra.   - Add method Graph:getPseudoDiameter().   - Hu2006: Scale coordinations of nodes in a coarse graph based on     the quotient of its pseudo diameters and that of the parent coarse     graph, as described in the paper.

--- generic/pgf/graphdrawing/core/lualayer/pgflibrarygraphdrawing-files.lua
+++ generic/pgf/graphdrawing/core/lualayer/pgflibrarygraphdrawing-files.lua
@@ -7,7 +7,7 @@
 --
 -- See the file doc/generic/pgf/licenses/LICENSE for more information
 
--- @release $Header: /cvsroot/pgf/pgf/generic/pgf/graphdrawing/core/lualayer/pgflibrarygraphdrawing-files.lua,v 1.1 2011/05/06 15:12:16 jannis-pohlmann Exp $
+-- @release $Header: /cvsroot/pgf/pgf/generic/pgf/graphdrawing/core/lualayer/pgflibrarygraphdrawing-files.lua,v 1.2 2011/05/09 20:40:53 jannis-pohlmann Exp $
 
 -- This file defines a list of initial files to load.
 
@@ -25,6 +25,9 @@ local files = {
    "edge",
    "graph",
    "traversal",
+   "fibonacci-heap",
+   "priority-queue",
+   "algorithms",
    "coarse-graph",
    "component-packing",
    "interface",
