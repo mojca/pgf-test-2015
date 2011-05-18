db6e287208603b5038eedf9642d6dc3bbd1b467a
Matthias Schulz <matthiasschulz@users.sourceforge.net>
2011-05-18 17:36:02 +0000

    added short overviews for the lua documentation edge and node classes in pgfmanual-en-base-graphdrawing and resolved a little bug (subgraphs related in lua)

--- generic/pgf/graphdrawing/core/lualayer/pgflibrarygraphdrawing-graph.lua
+++ generic/pgf/graphdrawing/core/lualayer/pgflibrarygraphdrawing-graph.lua
@@ -8,7 +8,7 @@
 --
 -- See the file doc/generic/pgf/licenses/LICENSE for more information
 
--- @release $Header: /cvsroot/pgf/pgf/generic/pgf/graphdrawing/core/lualayer/pgflibrarygraphdrawing-graph.lua,v 1.5 2011/05/15 14:15:45 jannis-pohlmann Exp $
+-- @release $Header: /cvsroot/pgf/pgf/generic/pgf/graphdrawing/core/lualayer/pgflibrarygraphdrawing-graph.lua,v 1.6 2011/05/17 12:37:57 tantau Exp $
 
 -- This file defines a graph class, which later represents user created
 -- graphs.
@@ -441,7 +441,7 @@ function Graph:subGraph(root, graph, visited)
     if canAdd then
       for v in table.value_iter(edge.nodes) do
         local translated = translate[v]
-        copy:addNode(translated)
+        graph:addNode(translated)
       end
       for node in table.value_iter(copy.nodes) do
         node:addEdge(copy)
