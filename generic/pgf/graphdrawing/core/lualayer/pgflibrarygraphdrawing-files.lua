9f1bd97c3440f4e7b226770469455d7707f50f41
Jannis Pohlmann <jannis-pohlmann@users.sourceforge.net>
2011-06-28 01:22:04 +0000

    graph drawing: Work on Ranking class and node ordering in GansnerKNV.
    The new Ranking class in core/lualayer can be used by all layered
    drawing algorithms in order to manage node ranks and node orderings in
    the different ranks.
    
    The GansnerKNV layered algorithm has been improved to use this new class
    in order to implement node ordering using the heuristic developed by
    Gansner et al that aims at reducing edge crossings in the layered graph
    drawing.

--- generic/pgf/graphdrawing/core/lualayer/pgflibrarygraphdrawing-files.lua
+++ generic/pgf/graphdrawing/core/lualayer/pgflibrarygraphdrawing-files.lua
@@ -7,7 +7,7 @@
 --
 -- See the file doc/generic/pgf/licenses/LICENSE for more information
 
--- @release $Header: /cvsroot/pgf/pgf/generic/pgf/graphdrawing/core/lualayer/pgflibrarygraphdrawing-files.lua,v 1.2 2011/05/09 20:40:53 jannis-pohlmann Exp $
+-- @release $Header: /cvsroot/pgf/pgf/generic/pgf/graphdrawing/core/lualayer/pgflibrarygraphdrawing-files.lua,v 1.3 2011/05/12 02:10:37 jannis-pohlmann Exp $
 
 -- This file defines a list of initial files to load.
 
@@ -34,6 +34,7 @@ local files = {
    "orientation",
    "positioning-helpers",
    "texboxregister",
+   "ranking",
 --   "test-path",
 --   "test-box",
 --   "test-vector",
