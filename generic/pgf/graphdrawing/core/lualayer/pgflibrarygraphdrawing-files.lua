a24b482909841d9607f5204177f107df5dcfb718
Jannis Pohlmann <jannis-pohlmann@users.sourceforge.net>
2011-06-30 12:55:38 +0000

    graph drawing: Create standalone NetworkSimplex class.
    We'll need the NetworkSimplex again for calculating the x coordinates
    in the GansnerKNV layered algorithm. For this it will have to be made a
    bit more generic though.

--- generic/pgf/graphdrawing/core/lualayer/pgflibrarygraphdrawing-files.lua
+++ generic/pgf/graphdrawing/core/lualayer/pgflibrarygraphdrawing-files.lua
@@ -7,7 +7,7 @@
 --
 -- See the file doc/generic/pgf/licenses/LICENSE for more information
 
--- @release $Header: /cvsroot/pgf/pgf/generic/pgf/graphdrawing/core/lualayer/pgflibrarygraphdrawing-files.lua,v 1.3 2011/05/12 02:10:37 jannis-pohlmann Exp $
+-- @release $Header: /cvsroot/pgf/pgf/generic/pgf/graphdrawing/core/lualayer/pgflibrarygraphdrawing-files.lua,v 1.4 2011/06/28 01:22:03 jannis-pohlmann Exp $
 
 -- This file defines a list of initial files to load.
 
@@ -35,6 +35,7 @@ local files = {
    "positioning-helpers",
    "texboxregister",
    "ranking",
+   "network-simplex",
 --   "test-path",
 --   "test-box",
 --   "test-vector",
