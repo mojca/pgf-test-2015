f82d2ca5a9de3aa9ae5308025a0259f24ddcf263
Jannis Pohlmann <jannis-pohlmann@users.sourceforge.net>
2011-07-06 21:50:03 +0000

    graph drawing: Parse node box coordinates prior to passing them to Lua.
    The TeX box coordinates have the format "123pt" and where passed to Lua
    unmodified before this commit. They are now resolved into point using
    \pgfmathsetmacro and the unit is dropped. As coordinates need to be
    passed back to the TeX layer in their original form, we later append the
    "pt" again.
    
    This fixes Node:getTexWidth() and Node:getTexHeight().

--- generic/pgf/graphdrawing/core/lualayer/pgflibrarygraphdrawing-node.lua
+++ generic/pgf/graphdrawing/core/lualayer/pgflibrarygraphdrawing-node.lua
@@ -8,7 +8,7 @@
 --
 -- See the file doc/generic/pgf/licenses/LICENSE for more information
 
--- @release $Header: /cvsroot/pgf/pgf/generic/pgf/graphdrawing/core/lualayer/pgflibrarygraphdrawing-node.lua,v 1.2 2011/05/10 14:21:00 tantau Exp $
+-- @release $Header: /cvsroot/pgf/pgf/generic/pgf/graphdrawing/core/lualayer/pgflibrarygraphdrawing-node.lua,v 1.3 2011/05/14 16:49:11 jannis-pohlmann Exp $
 
 -- This file defines a node class, used in the graph representation.
 
@@ -82,7 +82,7 @@ end
 -- @return Width of the node.
 --
 function Node:getTexWidth()
-	return self.tex.maxX - self.tex.minX
+	return math.abs(self.tex.maxX - self.tex.minX)
 end
 
 
@@ -92,7 +92,7 @@ end
 -- @return Height of the node.
 --
 function Node:getTexHeight()
-  return self.tex.maxY - self.tex.minY
+  return math.abs(self.tex.maxY - self.tex.minY)
 end
 
 
