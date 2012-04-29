10e8b4d334e528018df1c6120ac69d6ead1a08df
Christian Feuersaenger <cfeuersaenger@users.sourceforge.net>
2012-04-29 20:36:29 +0000

    fixed typos

--- generic/pgf/libraries/luamath/pgfluamath.parser.lua
+++ generic/pgf/libraries/luamath/pgfluamath.parser.lua
@@ -7,7 +7,7 @@
 --
 -- See the file doc/generic/pgf/licenses/LICENSE for more details.
 --
--- $Id: pgfluamath.parser.lua,v 1.12 2012/02/23 11:43:57 cjorssen Exp $	
+-- $Id: pgfluamath.parser.lua,v 1.13 2012/03/06 16:06:09 cjorssen Exp $	
 
 local pgfluamathparser = pgfluamathparser or {}
 
