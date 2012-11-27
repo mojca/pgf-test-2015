5fbd795125d54a606f7f16bf4ce40278dd799789
Till Tantau <tantau@users.sourceforge.net>
2012-11-27 17:24:26 +0000

    renamed all gd files so that they no longer have needlessly long file names

---
+++ generic/pgf/graphdrawing/lua/pgf/gd/model/library.lua
@@ -0,0 +1,15 @@
+-- Copyright 2012 by Till Tantau
+--
+-- This file may be distributed an/or modified
+--
+-- 1. under the LaTeX Project Public License and/or
+-- 2. under the GNU Public License
+--
+-- See the file doc/generic/pgf/licenses/LICENSE for more information
+
+--- @release $Header: /cvsroot/pgf/pgf/generic/pgf/graphdrawing/lua/pgf/gd/model/pgf.gd.model.library.lua,v 1.1 2012/11/20 23:32:04 tantau Exp $
+
+
+
+-- Load declarations from:
+require "pgf.gd.model.Hyperedge"
