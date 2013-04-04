7f447b54b8c70e88a4132991a2c49723faac24b4
Till Tantau <tantau@users.sourceforge.net>
2013-04-04 20:43:30 +0000

    *** empty log message ***

---
+++ doc/generic/pgf/lua/pgf/manual.lua
@@ -0,0 +1,18 @@
+-- Copyright 2013 by Till Tantau
+--
+-- This file may be distributed an/or modified
+--
+-- 1. under the LaTeX Project Public License and/or
+-- 2. under the GNU Public License
+--
+-- See the file doc/generic/pgf/licenses/LICENSE for more information
+
+-- @release $Header: /cvsroot/pgf/pgf/generic/pgf/graphdrawing/lua/pgf/gd/control/LayoutPipeline.lua,v 1.7 2013/03/15 15:04:41 tantau Exp $
+
+
+
+local manual = {}
+
+require 'pgf'.manual = manual
+
+return manual
